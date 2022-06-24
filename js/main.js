dafff977c5a4f1080d4d0b07680d3924122fd095
Yesy Magana <yesy.magana@yahoo.com>
2022-06-23 21:19:58 -0700

    image change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const userName = prompt('Hi Friend, what is your name?')
 console.log(userName);
 
+const image = document.querySelector('img')
+
+image.src = 'https://www.pngitem.com/pimgs/m/531-5319393_disney-winnie-the-pooh-clip-winnie-the-pooh.png'
+
 if (userName == null || userName == '') {
   txt = 'Click on the button below to ask me anything';
 } else {
@@ -32,4 +36,6 @@ function getFortune() {
   newMessage.setAttribute('id', "message");
   newMessage.innerHTML = "\"" + fortune + "\"";
   parent.appendChild(newMessage);
+
+  image.src = 'https://www.pngitem.com/pimgs/m/185-1856555_winnie-the-pooh-clipart-png-winnie-the-pooh.png'
 }
